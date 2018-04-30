0d20b810ceb7df23686a19a65e49ebb4f7300af9
Zach <zacharyaclark@gmail.com>
2018-04-30 00:03:54 -0500

    API Call

--- app/src/main/java/com/example/yaboyzc/myapplication/getWord.java
+++ app/src/main/java/com/example/yaboyzc/myapplication/getWord.java
@@ -1,13 +1,82 @@
 package com.example.yaboyzc.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.VoiceInteractor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.spec.ECField;
 
 public class getWord extends AppCompatActivity {
+
+    private static final String TAG = "Dictionary";
+
+    private static RequestQueue requestQueue;
+
+    //private static String word = theWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // set up queue for request
+        requestQueue = Volley.newRequestQueue(this);
+
+        // load main layout
         setContentView(R.layout.activity_get_word);
+
+        final Button startAPICall = findViewById(R.id.startAPICall);
+        startAPICall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Start API button clicked");
+                startAPICall();
+            }
+        });
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        //progressBar.setVisibility(View.);
+
+    }
+
+    /**
+     * make API call
+     */
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://od-api.oxforddictionaries.com/api/v1",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+
+                        }
+                        }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(final VolleyError error) {
+                            Log.w(TAG, error.toString());
+                            }
+                    });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
