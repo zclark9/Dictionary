c11e521060adfea5c1919966d17e51e92f87bbd4
Zach <zacharyaclark@gmail.com>
2018-04-30 20:39:59 -0500

    This one

--- app/src/main/java/com/example/yaboyzc/myapplication/getWord.java
+++ app/src/main/java/com/example/yaboyzc/myapplication/getWord.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import com.android.volley.Request;
@@ -17,8 +18,13 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
-import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import android.os.AsyncTask;
+
+import java.net.HttpURLConnection;
 import java.security.spec.ECField;
 
 public class getWord extends AppCompatActivity {
@@ -34,49 +40,98 @@ public class getWord extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // set up queue for request
-        requestQueue = Volley.newRequestQueue(this);
+        //requestQueue = Volley.newRequestQueue(this);
 
         // load main layout
-        setContentView(R.layout.activity_get_word);
+        //setContentView(R.layout.activity_get_word);
 
         final Button startAPICall = findViewById(R.id.startAPICall);
+        final EditText word = findViewById(R.id.theWord);
         startAPICall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "Start API button clicked");
-                startAPICall();
+                Log.v("EditText", word.getText().toString());
+                new CallbackTask().execute(inflections());
+                //startAPICall();
             }
         });
-
         ProgressBar progressBar = findViewById(R.id.progressBar);
         //progressBar.setVisibility(View.);
-
     }
 
-    /**
-     * make API call
-     */
-    void startAPICall() {
-        try {
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                    Request.Method.GET,
-                    "https://od-api.oxforddictionaries.com/api/v1",
-                    null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(final JSONObject response) {
-                            Log.d(TAG, response.toString());
+    private String inflections() {
+        final String language = "en";
+        final String word = "Change";
+        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
+        return "https://od-api.oxforddictionaries.com:443/api/v1/inflections/" + language + "/" + word_id;
+    }
 
-                        }
-                        }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(final VolleyError error) {
-                            Log.w(TAG, error.toString());
+    private class CallbackTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            //TODO: replace with your own app id and app key
+            final String app_id = "cccd15dc";
+            final String app_key = "f51721ace52300f02637f56e4865d4d0";
+            try {
+                URL url = new URL(params[0]);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("Accept", "application/json");
+                urlConnection.setRequestProperty("app_id", app_id);
+                urlConnection.setRequestProperty("app_key", app_key);
+
+                // read the output from the server
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+
+                return stringBuilder.toString();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return e.toString();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            System.out.println(result);
+        }
+
+
+        /**
+         * make API call
+         */
+        void startAPICall() {
+            try {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        "https://od-api.oxforddictionaries.com/api/v1",
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(final JSONObject response) {
+                                Log.d(TAG, response.toString());
+
                             }
-                    });
-            requestQueue.add(jsonObjectRequest);
-        } catch (Exception e) {
-            e.printStackTrace();
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(final VolleyError error) {
+                        Log.w(TAG, error.toString());
+                    }
+                });
+                requestQueue.add(jsonObjectRequest);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
